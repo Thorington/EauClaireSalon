763126791cb73337cb9f227f1f5896a2c68ae1cb
Claire Thorington <claire.thorington@gmail.com>
2022-07-29 14:25:57 -0700

    add styling, navigation

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -3,9 +3,9 @@ namespace HairSalon.Models
   public class Client
   {
     public int ClientId { get; set; }
+    public int StylistId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public int StylistId { get; set; }
     public virtual Stylist stylist { get; set; }
   }
 }
